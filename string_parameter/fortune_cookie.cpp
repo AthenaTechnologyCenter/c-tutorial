bc621aa32a6811c0372b2831e4eb48ee43387651
minhtv1811 <minhtv1811@gmail.com>
2022-09-18 10:19:03 +0700

    add display array address

--- string_parameter/fortune_cookie.cpp
+++ string_parameter/fortune_cookie.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
-	char quote[] = "Cookies make you fat";
-	fortune_cookie(quote);
-    return 0;
-}
-
 void fortune_cookie(char msg[])
 {
 	printf("Message reads: %s\n", msg);
 	printf("msg occupies %i bytes\n", sizeof(msg));
+}
+
+int main()
+{
+	char quote[] = "Cookies make you fat";
+	printf("The quote string is stored at: %p\n", quote);
+	fortune_cookie(quote);
+    return 0;
 }
 
